2e219fc2d7a3d8dd9523a41858cdb4f0b51370c3
kishan <gondaliyak003@gmail.com>
2023-04-21 23:25:55 +0530

    create context file and working on Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,40 @@
+// import { useContext, useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
+import { TbSearch } from "react-icons/tb"
+import { CgShoppingCart } from "react-icons/cg"
+import { AiOutlineHeart } from "react-icons/ai"
+
+// import Search  from "./Search/Search"
+// import Cart from "../Cart/Cart"
+// import {Context} from "../../utils/context"
+
 import "./Header.scss";
 const Header = () => {
-    return <div>Header</div>;
-};
+    console.log("header")
+    return (
+        <>
+            <header className="main-header">
+                <div className="header-content">
+                    <ul className="left">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Categories</li>
+                    </ul>
+                </div>
+                <div className="center">
+                    ONE-CLICK STORE
+                </div>
+                <div className="right">
+                    <TbSearch />
+                    <AiOutlineHeart />
+                    <span className="cart-icon">
+                        <CgShoppingCart />
+                    </span>
+                </div>
+            </header>
+        </>
+    );
+}
 
 export default Header;
